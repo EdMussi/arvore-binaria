ce9d576f2dbb1ccd352fdb1dd742348a8d59dd50
Leo-Cavalli <leocavalli0203@gmail.com>
2023-09-24 23:29:27 -0300

    Comit Deletion

---
+++ Deletion.java
@@ -0,0 +1,34 @@
+class Deletion {
+    static TreeNode delete(TreeNode root, int key) {
+        if (root == null) {
+            return root;
+        }
+
+        if (key < root.key) {
+            root.left = delete(root.left, key);
+        } else if (key > root.key) {
+            root.right = delete(root.right, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            root.key = minValue(root.right);
+
+            root.right = delete(root.right, root.key);
+        }
+
+        return root;
+    }
+
+    private static int minValue(TreeNode root) {
+        int minValue = root.key;
+        while (root.left != null) {
+            minValue = root.left.key;
+            root = root.left;
+        }
+        return minValue;
+    }
+}
